ef2a9529de2c3cf234af741492345d465dc524ea
milene <mimi0003@stud.kea.dk>
2025-12-11 12:37:04 +0100

    url tilfoejet

--- JS/supabase.js
+++ JS/supabase.js
@@ -3,8 +3,8 @@
 
 eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InllcXN0cGV5YXd6bW1qbXB5YWp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU0MjU3NDUsImV4cCI6MjA4MTAwMTc0NX0.BjkdOimfoSgoBzsbHIJyPI7lkDCz7GZYZFuK_iAFvDc;
 
-const url = "https://yeqstpeyawzmmjmpyajy.supabase.co/rest/v1/holdoversigt";
-const url = "https://qeoxxkgbtrpnlhjhgkqx.supabase.co/rest/v1/Cases?";
+const url = "https://yeqstpeyawzmmjmpyajy.supabase.co/rest/v1/holdoversigt?";
+
 const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InllcXN0cGV5YXd6bW1qbXB5YWp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU0MjU3NDUsImV4cCI6MjA4MTAwMTc0NX0.BjkdOimfoSgoBzsbHIJyPI7lkDCz7GZYZFuK_iAFvDc;";
 const options = {
   headers: {
